0a2de1428f843f91f7be71e75abc023187787e93
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-07 15:04:05 -0800

    add methods for venus, mercury, and mars calculations.

--- src/calc.js
+++ src/calc.js
@@ -1,5 +1,23 @@
 export class User {
   constructor(age) {
     this.age = age;
-  } 
+  }
+  
+  calcVenus() {
+    let currentAge = this.age;
+    let venus = Math.floor(currentAge/.62);
+    return venus;
+  }
+
+  calcMercury() {
+    let currentAge = this.age;
+    let mercury = Math.floor(currentAge/.24);
+    return mercury;
+  }
+
+  calcMars() {
+    let currentAge = this.age;
+    let mars = Math.floor(currentAge/1.88);
+    return mars;
+  }
 }
