a9053738c081b8eace7f1fb0032293ea3f3b4a6e
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-07 15:06:59 -0800

    test for an inaccurate, then positive input for mars age calculation.

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -7,7 +7,22 @@ describe ('User', () => {
     newUser = new User(35);
   });
 
-  test('should test that the user has entered a number stores as their age in Earth years.', () => {
+  test ('should test that the user has entered a number stores as their age in Earth years.', () => {
     expect(newUser.age).toEqual(35);
+  });
+
+  test ('should test the users age in Venus years', () => {
+    const venus = newUser.calcVenus();
+    expect(venus).toEqual(56);
+  });
+
+  test ('should test the users age in Venus years', () => {
+    const mercury = newUser.calcMercury();
+    expect(mercury).toEqual(145);
+  });
+
+  test ('should test the users age in Mars years', () => {
+    const mars = newUser.calcMars();
+    expect(mars).toEqual(18);
   });
 });
